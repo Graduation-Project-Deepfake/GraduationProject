190de26b76625378fc78c2f8b68afe8cbd117a4b
Omar Ahmed <75138543+OmarAhmed0001@users.noreply.github.com>
2023-07-04 20:03:42 +0300

    padding

--- server_client/controllers/DeepfakeController.ts
+++ server_client/controllers/DeepfakeController.ts
@@ -4,33 +4,32 @@ import asyncHandler from 'express-async-handler';
 import { Server } from 'socket.io';
 import { uploadSingleVideo } from '../middlewares/uploadVideoMiddleware';
 import deepfake from '../models/deepfakeModel';
- // Express app
+// Express app
 const app = express();
- // Socket.io connection
- const serverio = require('http').createServer(app);
- const io = new Server(serverio, {
-     cors: {
-         origin: '*',
-         methods: '*',
-         credentials: true,
-     },
- });
- let predict="";
- let video="";
- io.on('connection', async (socket) => {
-     socket.on('prediction', async (prediction) => {
-        predict=prediction;
-         console.log(prediction);
-     });
- 
-     socket.on('disconnect', () => {
-         console.log('user disconnected');
-     });
- });
- serverio.listen(3000, () => {
-     console.log('server is running......');
- });
+// Socket.io connection
+const serverio = require('http').createServer(app);
+const io = new Server(serverio, {
+    cors: {
+        origin: '*',
+        methods: '*',
+        credentials: true,
+    },
+});
+let predict = '';
+let video = '';
+io.on('connection', async (socket) => {
+    socket.on('prediction', async (prediction) => {
+        predict = prediction;
+        console.log(prediction);
+    });
 
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+serverio.listen(3000, () => {
+    console.log('server is running......');
+});
 
 // @desc    Upload Single image for deepfake
 // @route   POST /api/v1/deepfake/:id/deepfake
@@ -51,8 +50,8 @@ export const createdeepfake = asyncHandler(
         // Process the uploaded video here (e.g., save to a database, perform operations, etc.)
         req.body.video = uploadedFile.originalname;
         console.log(req.body.video);
-        video=req.body.video;
-         io.emit('data',req.body.video)
+        video = '1.mp4';
+        io.emit('data', req.body.video);
         const document = await deepfake.create({ video: req.body.video });
         res.status(201).json({ data: document });
     }
@@ -61,16 +60,22 @@ export const createdeepfake = asyncHandler(
 // @desc    Get Single video for deepfake
 // @route   Get /api/v1/deepfake
 // @access  Public
-let preprocessed_images = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"];
-let faces_cropped_images=["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"];
+let preprocessed_images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
+let faces_cropped_images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
 const no_faces = false;
 export const getPredict = (req: Request, res: Response, next: NextFunction) => {
-    predict="REAL";
+    predict = 'REAL';
     if (!predict) {
         setTimeout(() => {
-          getPredict(req, res, next);
+            getPredict(req, res, next);
         }, 100);
-       } else {
-        res.render('deepfake', { message: predict ,preprocessed_images,faces_cropped_images,no_faces ,video});
-       }
+    } else {
+        res.render('deepfake', {
+            message: predict,
+            preprocessed_images,
+            faces_cropped_images,
+            no_faces,
+            video,
+        });
+    }
 };
